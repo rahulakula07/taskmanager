7c165f0c1f69551820294a2362a1b6e12aba62e6
rahulakula07 <rahulakula4265@gmail.com>
2025-06-10 12:28:58 +0530

    removed vite config link

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: ["http://localhost:8800","https://taskmanager-three-sigma.vercel.app"],
+        // target: ["http://localhost:8800","https://taskmanager-three-sigma.vercel.app"],
+        target: ["http://localhost:8800"],
         changeOrigin: true,
       },
     },
